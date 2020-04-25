197da7b6f697a6ac5594a5f246a10768cff84fb9
Aleksej <avoroncovs@gmail.com>
2020-04-25 22:30:23 +0300

    removed comment

--- src/main/java/com/paymentprocessor/dto/PaymentDTO.java
+++ src/main/java/com/paymentprocessor/dto/PaymentDTO.java
@@ -30,14 +30,4 @@ public class PaymentDTO {
     String details;
     String creditorBankBic;
 
-    /*
-    * Client should be able to create payment of one of 3 types - TYPE1, TYPE2, TYPE3. Fields 'amount' (positive decimal),
-    * 'currency' (EUR or USD), 'debtor_iban' and 'creditor_iban' (texts) are mandatory for all types.
-Additional type-specific requirements:
-TYPE1 is only applicable for EUR payments, has additional field 'details' (text) which is mandatory;
-TYPE2 is only applicable for USD payments, has additional field ‘details’ (text) which is optional.
-TYPE3 is applicable for payments in both EUR and USD currency, has additional field for creditor bank BIC code (text)
- which is mandatory.
-    * */
-
 }
